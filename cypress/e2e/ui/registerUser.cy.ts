48a8486167eb923eadc58f9b8a45874c2f0b79e0
xosalasb0490x <osalasb0490@gmail.com>
2026-04-04 00:37:32 -0600

    registeruser1

--- cypress/e2e/ui/registerUser.cy.ts
+++ cypress/e2e/ui/registerUser.cy.ts
@@ -1,6 +1,10 @@
 describe('Register Functionality (https://automationexercise.com)', () => {
     it('Should register a new user successfully', () => {
-
+        cy.fixture('credentials').then((a) => {
+            cy.registerUser(a.informationUser);
+            cy.get('[data-qa="account-created"]')
+                .should('be.visible');
         });
+    });
 
 });
--- cypress/factories/userfactorie.ts
+++ cypress/factories/userfactorie.ts
@@ -1,1 +1,18 @@
-import {faker } from '@faker-js/faker'; 
+import {faker} from '@faker-js/faker';
+
+export interface User {
+  username: string
+  password: string
+  email: string
+}
+
+export const buildUser = (overrides?: Partial<User>): User => {
+
+  return {
+    username: faker.internet.username(),
+    password: faker.internet.password(),
+    email: faker.internet.email(),
+    ...overrides
+  }
+
+}
